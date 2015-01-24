5e35360f1967156a6283480cbfa2bf96c778d66d
Malte-Maurice Dreyer <dreyer.maltem@gmail.com>
2015-01-24 23:32:03 +0000

    Fix for multiple upper case chrs in a row

--- case-camel.js
+++ case-camel.js
@@ -1,8 +1,35 @@
-var regExpParse = /((?:^|[A-Z])[A-Z]*[^A-Z]*)/g,
-    regExpIs = /[A-Z][a-z]/;
-
 function toLowerCase(str) {
     return str.toLowerCase();
+}
+
+function parse(str) {
+    var chrs = str.split(""),
+        chrsLowerCase = str.toLowerCase().split(""),
+        start = 0,
+        ret = [],
+        lastUpper, i, length;
+
+    if(chrs[0] !== chrsLowerCase[0]) {
+        lastUpper = 0;
+    }
+
+    for(i = 1, length = chrs.length; i < length; ++i) {
+        if(chrs[i] === chrsLowerCase[i]) {
+            if(lastUpper && lastUpper !== start) {
+                ret.push(chrs.slice(start, lastUpper).join(""));
+                start = lastUpper;
+            }
+        } else {
+            if(lastUpper !== i - 1) {
+                ret.push(chrs.slice(start, i).join(""));
+                start = i;
+            }
+            lastUpper = i;
+        }
+    }
+    ret.push(chrs.slice(start).join(""));
+
+    return ret;
 }
 
 module.exports = {
@@ -14,7 +41,13 @@ module.exports = {
             return [];
         }
 
-        return String(val).match(regExpParse);
+        val = String(val);
+
+        if(val.length < 2) {
+            return [val];
+        }
+
+        return parse(val);
     },
     stringify: function(val) {
         if(typeof(val) === "undefined" || val === null) {
@@ -30,6 +63,6 @@ module.exports = {
         return val.charAt(0).toUpperCase() + val.slice(1);
     },
     is: function(val) {
-        return typeof(val) !== "undefined" && val !== null && regExpIs.test(String(val));
+        return typeof(val) !== "undefined" && val !== null;
     }
 };
