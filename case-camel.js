04fef70bdd815df4e7969613bb9a900822472705
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-04-18 18:06:10 +0000

    Fix remaining standard errors

--- case-camel.js
+++ case-camel.js
@@ -1,4 +1,4 @@
-export {default as is} from "./lib/is"
-export {default as parseSameCase} from "./lib/parse-same-case"
-export {default as parse} from "./lib/parse"
-export {default as stringify} from "./lib/stringify"
+export {default as is} from './lib/is'
+export {default as parseSameCase} from './lib/parse-same-case'
+export {default as parse} from './lib/parse'
+export {default as stringify} from './lib/stringify'
