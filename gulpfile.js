47527f0acc78757fec550212cf1d280e117d28ee
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-04-18 17:52:29 +0000

    Use standard style in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
-var gulp = require('gulp'),
-  babel = require('gulp-babel'),
-  sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp')
+var babel = require('gulp-babel')
+var sourcemaps = require('gulp-sourcemaps')
 
-gulp.task('build', function() {
+gulp.task('build', function () {
   return gulp.src(['{case-camel.js,lib/*.js}'])
     .pipe(sourcemaps.init())
       .pipe(babel())
     .pipe(sourcemaps.write('../maps'))
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(gulp.dest('dist'))
+})
 
-gulp.task('test', function() {
-  var mocha = require('gulp-mocha');
+gulp.task('test', function () {
+  var mocha = require('gulp-mocha')
 
   return gulp.src('test/index.js', {read: false})
     .pipe(mocha({
       reporter: 'nyan'
-    }));
-});
+    }))
+})
